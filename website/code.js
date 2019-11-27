dd4535bcf6a9b1a310de26550246eb8cc93728d1
HiimLuki <32770067+HiimLuki@users.noreply.github.com>
2019-11-28 00:21:29 +0100

    Kommentare hinzugefügt

--- website/code.js
+++ website/code.js
@@ -1,5 +1,6 @@
 function initialize(){
 
+    //Variablen deklarieren
     var allowsound = false;
     var playStopButton = document.getElementById("playStopButton");
     var socket = io.connect();
@@ -8,40 +9,51 @@ function initialize(){
 
 		document.getElementById("label").innerHTML = `Tierform: ${data.x}`;
 
+        //Daten von Python in Variable gespeichert
 		var tierform = `${data.x}`;
 	
 		if(tierform=="Kreis"){
+            //Darstellen des Bildes Kreis
             document.getElementById("animal_img").innerHTML = '<img src="images/kreis.jpg" alt="" width="320" height="240" />';
+            //Wenn Button gedrückt kann Sound abgespielt werden (Disablen vom Sound damit er nicht unendlich oft kommt)
             if(allowsound==true){
-                    playCat();
-		    allowsound=false;
+                playCat();
+                allowsound=false;
             }
             
 		}
 		else if(tierform=="Katze"){
+            //Darstellen des Bildes Katze
             document.getElementById("animal_img").innerHTML = '<img src="images/katze.jpg" alt="" width="320" height="240" />';
+            //Enablen von Button Katze
+            //document.getElementById("katze").disabled = false;
+            //Wenn Button gedrückt kann Sound abgespielt werden (Disablen vom Sound damit er nicht unendlich oft kommt)
             if(allowsound==true){
                 playCat();
-		    allowsound=false;
+                allowsound=false;
             }
 		}
 		else if(tierform=="Elefant"){
+            //Darstellen des Bildes Elefant
             document.getElementById("animal_img").innerHTML = '<img src="images/elefant.jpg" alt="" width="320" height="240" />';
+            //Enablen von Button Elefant
+            //document.getElementById("elefant").disabled = false;
+            //Wenn Button gedrückt kann Sound abgespielt werden (Disablen vom Sound damit er nicht unendlich oft kommt)
             if(allowsound==true){
                 playElefant();
-		    allowsound=false;
+                allowsound=false;
             }
 		}
 		
     });
     
-    
+    //Listener, damit GOOGLE erlaubt wird, das Sound abgespielt werden darf (BENUTZEREINGABE dank neuer RICHTLINIE)
     playStopButton.addEventListener("click", function (e) {
         allowsound = true;
     });
 }
-
-   
+ 
+//Laden und abspielen vom Katzensound
 function playCat() {
 
     var context = new AudioContext();
@@ -52,8 +64,8 @@ function playCat() {
 
     sound.play();
 }
-
-  
+ 
+//Laden und abspielen vom Elefantensound
 function playElefant() {
 
     var context = new AudioContext();
