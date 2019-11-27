b78e2a808fea1b095bb320e518e77e0b3e8398c1
HiimLuki <32770067+HiimLuki@users.noreply.github.com>
2019-11-27 23:29:16 +0100

    index und code geupdated

--- website/code.js
+++ website/code.js
@@ -1,29 +1,64 @@
 function initialize(){
-	
-	var socket = io.connect();
+
+    var allowsound = false;
+    var playStopButton = document.getElementById("playStopButton");
+    var socket = io.connect();
 	
 	socket.on("position", function(data){
 
 		document.getElementById("label").innerHTML = `Tierform: ${data.x}`;
 
-
-		//Funktioniert noch nicht
 		var tierform = `${data.x}`;
-
-		console.log(tierform);
-		while(true){
+	
 		if(tierform=="Kreis"){
-			document.getElementById("animal_img").innerHTML = "Kreis";
+            document.getElementById("animal_img").innerHTML = '<img src="images/kreis.jpg" alt="" width="320" height="240" />';
+            if(allowsound==true){
+                    playCat();
+            }
+            
 		}
 		else if(tierform=="Katze"){
-			document.getElementById("animal_img").innerHTML = '<img src="images/katze.jpg" alt="" width="240" height="71" />';
+            document.getElementById("animal_img").innerHTML = '<img src="images/katze.jpg" alt="" width="320" height="240" />';
+            if(allowsound==true){
+                playCat();
+            }
 		}
 		else if(tierform=="Elefant"){
-			document.getElementById("animal_img").innerHTML = "Elefant";
+            document.getElementById("animal_img").innerHTML = '<img src="images/elefant.jpg" alt="" width="320" height="240" />';
+            if(allowsound==true){
+                playElefant();
+            }
 		}
-	}
-	});
-
-
-
+		
+    });
+    
+    
+    playStopButton.addEventListener("click", function (e) {
+        allowsound = true;
+    });
 }
+
+   
+function playCat() {
+
+    var context = new AudioContext();
+    var sound = new Audio("sounds/katze.mp3");
+    sound.crossOrigin = "anonymous";
+    var source = context.createMediaElementSource(sound);
+    source.connect(context.destination);
+
+    sound.play();
+}
+
+  
+function playElefant() {
+
+    var context = new AudioContext();
+    var sound = new Audio("sounds/elefant.mp3");
+    sound.crossOrigin = "anonymous";
+    var source = context.createMediaElementSource(sound);
+    source.connect(context.destination);
+
+    sound.play();
+}
+
